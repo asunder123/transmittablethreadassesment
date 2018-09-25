569ba022e0e3ad1cf81fb9d1041b0ea0dd0c1482
Jerry Lee <oldratlee@gmail.com>
2018-09-25 23:37:48 +0800

    WIP

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.alibaba.ttl {
+    exports com.alibaba.ttl;
+    exports com.alibaba.ttl.threadpool;
+
+    requires java.instrument;
+    requires java.logging;
+    requires javassist;
+}
